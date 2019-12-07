0eb7f91fe4f05da433800a328b0807fd75eb5524
Tharit Tiewjarernsopa <tharit.t@ginkgosoft.co.th>
2019-12-08 03:19:03 +0700

    no message

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import express, { static } from 'express';
+const express = require('express');
 const app = express();
 
-app.use(static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
