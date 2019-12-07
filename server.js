4f7f52512bc6afab2bc3264d7be65b33171c0fac
Tharit Tiewjarernsopa <tharit.t@ginkgosoft.co.th>
2019-12-08 04:06:09 +0700

    no message

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 //Install express server
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
+
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/pokemon'));
-
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 8080);
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname, '/dist/pokemon/index.html'));
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
